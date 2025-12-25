bd10d52c0edcc03c0c1a93ef383145da2d25453e
Lorenzo Celli <lorenzo.celli00@gmail.com>
2025-12-25 21:45:49 +0100

    Add missing import

--- src/architectures/__init__.py
+++ src/architectures/__init__.py
@@ -3,6 +3,7 @@ from .cnn import CNN
 from .lstm import LSTM
 from .mamba import Mamba
 from .mlp import MLP
+from .mlp import UnbiasedMLP
 from .resnet import Resnet
 from .vit import VIT
 from .transformer import Transformer
