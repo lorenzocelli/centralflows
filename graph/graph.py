5ce6338df01f3f16fa4b1fdfb4d41b594c96ccf0
Lorenzo Celli <lorenzo.celli00@gmail.com>
2026-01-18 15:12:42 +0100

    Update graph script

--- graph/graph.py
+++ graph/graph.py
@@ -23,17 +23,19 @@ parser.add_argument("--eig-max", type=float, default=-1, help="Maximum y-axis fo
 
 args = parser.parse_args()
 
+plt.style.use("ggplot")
+
 plt.rcParams["font.family"] = "sans-serif"
 plt.rcParams["font.sans-serif"] = ["CMU Sans Serif"]
 plt.rcParams["font.weight"] = "medium"
 plt.rcParams["axes.unicode_minus"] = False
 
-plt.style.use("ggplot")
+COLUMN_WIDTH = 3.287
 
 if args.raw:
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(15, 5))
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(4 * 2.5, COLUMN_WIDTH))
 else:
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 5))
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(3 * 2.5, COLUMN_WIDTH))
 
 lines = []
 
@@ -86,18 +88,28 @@ for i, (x, y, color, label) in enumerate(lines):
 if args.eig_max > 0:
     ax3.set_ylim(bottom=0, top=args.eig_max)
 
-ax1.legend()
-ax2.legend()
-ax3.legend()
+ax3.axhline(2, color="black", linewidth=0.8, linestyle="--")
+
+ax3.axvline(600, color="#188487", linewidth=0.8, linestyle="--")
+ax3.axvline(1000, color="#188487", linewidth=0.8, linestyle="--")
+ax3.annotate('unfreeze', xy=(500, 5), color='#188487', horizontalalignment='right', verticalalignment='center', rotation=90)
+ax3.annotate('freeze', xy=(1000, 5), color='#188487', horizontalalignment='right', verticalalignment='center', rotation=90)
+
+# ax1.legend()
+# ax2.legend()
+# ax3.legend()
 
 ax1.set_title("Training Loss", fontsize=11, loc="left")
 ax2.set_title("Gradient Norm Squared", fontsize=11, loc="left")
 ax3.set_title("Effective Hessian Eigenvalues", fontsize=11, loc="left")
 
 if args.raw:
-    ax4.legend()
+    # ax4.legend()
     ax4.set_title("Raw Hessian Eigenvalues", fontsize=11, loc="left")
 
-fig.tight_layout()
+# Align title left
+fig.suptitle("Muon with Frozen/Unfrozen Preconditioner lr=1e-4", fontsize=11, fontweight="bold", y=0.94, x=0.043, ha="left")
 
+fig.tight_layout()
+plt.savefig(f"{'_'.join(args.exp)}.png", dpi=300, bbox_inches="tight", pad_inches=0)
 plt.show()
