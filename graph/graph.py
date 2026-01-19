4a4db6d89243465e1f94970310f9828ae9bf2379
Lorenzo Celli <lorenzo.celli00@gmail.com>
2026-01-19 19:43:01 +0100

    Update graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -7,7 +7,10 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description="Plot graphs")
 parser.add_argument(
-    "-e", "--exp", nargs="+", required=True, help="List of experiment directories"
+    "-e", "--exp", nargs="+", required=True, type=str, help="List of experiment directories"
+)
+parser.add_argument(
+    "-l", "--legends", nargs="+", required=False, type=str, help="List of legends for the experiments"
 )
 parser.add_argument(
     "--max-iter", type=int, default=-1, help="Maximum number of iterations to plot"
@@ -31,6 +34,7 @@ plt.rcParams["font.family"] = "sans-serif"
 plt.rcParams["font.sans-serif"] = ["CMU Sans Serif"]
 plt.rcParams["font.weight"] = "medium"
 plt.rcParams["axes.unicode_minus"] = False
+plt.rcParams["legend.fontsize"] = 8
 
 if args.raw:
     fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(4 * 2.5, 3.287))
@@ -38,8 +42,9 @@ else:
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(3 * 2, 3.287))
 
 lines = []
+labels = [args.legends[i] if args.legends and i < len(args.legends) else args.exp[i] for i in range(len(args.exp))]
 
-for exp_dir in args.exp:
+for i, exp_dir in enumerate(args.exp):
     data = os.path.join(exp_dir, "data.hdf5")
     exp = os.path.basename(os.path.normpath(exp_dir))
 
@@ -53,32 +58,32 @@ for exp_dir in args.exp:
         i_min = args.min_iter if args.min_iter > 0 else 0
         i_max = args.max_iter if args.max_iter > 0 else len(step)
 
-        ax1.plot(step[i_min:i_max], loss_train[i_min:i_max], label=f"Train {exp}")
+        ax1.plot(step[i_min:i_max], loss_train[i_min:i_max], label=labels[i])
         # ax1.plot(step[i_min:i_max], loss_test[i_min:i_max], label=f"Test {exp}")
-        ax2.plot(step[i_min:i_max], grad_sq[i_min:i_max], label=exp)
+        ax2.plot(step[i_min:i_max], grad_sq[i_min:i_max], label=labels[i])
 
         if "effective_hessian_eigs" in discrete:
             heigs = discrete["effective_hessian_eigs"]
             raw_heigs = discrete["hessian_eigs"]
 
-            for i in range(heigs.shape[1]):
+            for j in range(heigs.shape[1]):
                 weights = np.ones(args.smoothing) / args.smoothing
-                heigs_avg = np.convolve(heigs[:, i], weights, mode="same")[i_min:i_max]
-                (line,) = ax3.plot(step[i_min:i_max], heigs[i_min:i_max, i], alpha=0.25)
+                heigs_avg = np.convolve(heigs[:, j], weights, mode="same")[i_min:i_max]
+                (line,) = ax3.plot(step[i_min:i_max], heigs[i_min:i_max, j], alpha=0.25)
                 lines.append(
                     (
                         step[i_min:i_max],
                         heigs_avg,
                         line.get_color(),
-                        f"Eig {i + 1} {exp}",
+                        labels[i],
                     )
                 )
 
             if args.raw:
                 ax4.plot(
                     step[i_min:i_max],
-                    raw_heigs[i_min:i_max, i],
-                    label=f"Raw Eig {i + 1} {exp}",
+                    raw_heigs[i_min:i_max, 0],
+                    label=labels[i],
                 )
 
 for i, (x, y, color, label) in enumerate(lines):
@@ -107,5 +112,8 @@ if args.raw:
 fig.suptitle("Muon with Newton-Schulz Preconditioner lr=1e-3", fontsize=11, fontweight="bold", y=0.94, x=0.043, ha="left")
 
 fig.tight_layout()
-plt.savefig(f"{'_'.join(args.exp)}.png", dpi=300, bbox_inches="tight", pad_inches=0)
+if len(args.exp) == 1:
+    plt.savefig(f"{'_'.join(args.exp)}.png", dpi=300, bbox_inches="tight", pad_inches=0)
+else:
+    plt.savefig("training_dataset_size_comparison.png", dpi=300, bbox_inches="tight", pad_inches=0)
 plt.show()
